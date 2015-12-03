4f21c177c9db274cbdb168b0e5c7227c311b1889
Steve Cohen <steve@pinterest.com>
2015-12-03 09:50:07 -0800

    Switched to a more easily compilable fork of exometer
    Reviewers: jon, jean, kelsey
    
    Reviewed By: kelsey
    
    Differential Revision: https://phabricator.pinadmin.com/D60807

--- mix.exs
+++ mix.exs
@@ -8,29 +8,17 @@ defmodule Elixometer.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
     [ mod: {Elixometer.App, []},
       applications: [:logger, :exometer]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [
         {:meck, github: "eproxus/meck", tag: "0.8.2", override: true},
         {:edown, github: "uwiger/edown", tag: "0.7", override: true},
-        {:lager, github: "basho/lager", tag: "2.0.3", override: true},
-        {:exometer, github: "Feuerlabs/exometer", tag: "1.2"},
+        {:lager, github: "basho/lager", tag: "2.1.0", override: true},
+        {:exometer, github: "pspdfkit-labs/exometer"},
         {:netlink, github: "Feuerlabs/netlink", ref: "d6e7188e", override: true},
     ]
   end
