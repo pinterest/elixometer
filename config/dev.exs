4d502ecc7442eabfe007be35944f171c8bbf2455
Jon Parise <jon@pinterest.com>
2017-07-02 17:00:28 -0700

    Use keyword lists for lager configuration
    The previous syntax has been deprecated.

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :exometer_core, report: [reporters: []]
 config :lager, [
   log_root: 'log',
   handlers: [
-    lager_console_backend: :info,
+    lager_console_backend: [level: :info],
     lager_file_backend: [file: 'error.log',
                          level: :error,
                          formatter: :lager_default_formatter,
@@ -13,7 +13,7 @@ config :lager, [
     lager_file_backend: [file: 'console.log', level: :debug]
   ]]
 
-config :elixometer,  reporter: :exometer_report_tty,
+config :elixometer, reporter: :exometer_report_tty,
 update_frequency: 1000,
 env: Mix.env,
 metric_prefix: "elixometer"
