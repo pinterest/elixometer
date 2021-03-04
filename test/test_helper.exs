4d5143fe87c048fb1c46edc9fa1701f939a51a04
Jon Parise <jon@pinterest.com>
2021-03-04 11:48:10 -0800

    Remove the "Elixir 1.5" test tag support
    We already require Elixir 1.5 or later so this version check is no
    longer needed.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,1 @@
-# Exclude Elixir 1.5-tagged tests when running on earlier versions.
-if Version.compare(System.version(), "1.5.0-rc") == :lt do
-  ExUnit.configure(exclude: [elixir: 1.5])
-end
-
 ExUnit.start()
