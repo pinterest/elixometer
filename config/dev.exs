5155f4eca9eb420b467f9b4c4fb327e69e524837
Jon Parise <jon@pinterest.com>
2021-11-26 09:14:53 -0800

    Drop lager dependency
    This was previously used to manage log output from Erlang :error_logger.
    It may have also been a dependency of exometer_core at some point in the
    past, but I couldn't find a reference to it after some brief searching.
    
    Either way, :logger itself does the same job without the additional
    dependency.

--- config/dev.exs
+++ config/dev.exs
@@ -2,21 +2,11 @@ use Mix.Config
 
 config :exometer_core, report: [reporters: []]
 
-config :lager,
-  log_root: 'log',
-  handlers: [
-    lager_console_backend: [level: :info],
-    lager_file_backend: [
-      file: 'error.log',
-      level: :error,
-      formatter: :lager_default_formatter,
-      formatter_config: [:date, " ", :time, " [", :severity, "]", :pid, " ", :message, "\n"]
-    ],
-    lager_file_backend: [file: 'console.log', level: :debug]
-  ]
-
 config :elixometer,
   reporter: :exometer_report_tty,
   update_frequency: 1000,
   env: Mix.env(),
   metric_prefix: "elixometer"
+
+config :logger,
+  level: :info
